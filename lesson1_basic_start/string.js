03eb2d350cb944a42d6a6be30ccf970cd08ce93b
Gouri Tripathi <89586285+gouriTD@users.noreply.github.com>
2023-10-04 16:31:12 +0000

    strings

---
+++ lesson1_basic_start/string.js
@@ -0,0 +1,22 @@
+/*
+Strings are one of the most important data types in JS.
+
+1) stings can be defined as:
+let newString = "abc"
+OR
+let newString = new String('abc') ---> Here the string created is with a Class called string.
+
+2) There are different methods of importance with string like:
+    a) toLowerCase : converts string value to entirely lowercase.
+    b) toUpperCase : converts all string characters to uppercase.
+    c) trim : trims /removes all white spaces in and around a string.
+    d) different types of trim functions are trimLeft, trimRight, trimStart, trimEnd.
+    e) substr : creates a substring out of given string.
+    f) slice : this method creates a substr out of a given string, here start and end indexes are required. slice accepts negative
+    index value as well.
+    g) includes : returns true/false depending upon the value / char there in the string.
+    h) charAt : returns a character at a given index.
+    i) indexOf : returns an index of a given character.
+    j) replace: takes 2 strings, first one is the value to be replaced and second is the value with which to replace the string.
+    
+*/
